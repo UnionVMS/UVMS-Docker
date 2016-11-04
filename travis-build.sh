d52d03a273ba356a17d69402de75f847131d93f8
Oscar Djupfeldt <oscar.djupfeldt@havochvatten.se>
2016-11-04 14:25:52 +0100

    Removed travis-build.sh Added $TRAVIS_JOB_NUMBER to build

--- travis-build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-TIME=time
-docker pull uvms/postgres:9.3
-docker pull uvms/wildfly:8.2.0
-docker build --tag uvms/postgres-full:$TIME --no-cache postgres
-docker build --tag uvms/wildfly-full:$TIME --no-cache wildfly
