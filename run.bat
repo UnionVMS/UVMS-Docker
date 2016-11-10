d047d37d207da3ac44aaefb7a80b5487d94a0dd4
Oscar Djupfeldt <oscar.djupfeldt@havochvatten.se>
2016-11-10 14:28:30 +0100

    Missed an a at the end of data

--- run.bat
+++ run.bat
@@ -1,7 +1,7 @@
 docker rm wildfly activemq postgres
 docker network rm uvms
 docker network create uvms
-docker run -it -p 61616:61616 -p 8161:8161 --name activemq --net-alias activemq --net=uvms -v c:/uvms/activemq:/opt/jboss/activemq/dat -d uvms/activemq:5.13.2
+docker run -it -p 61616:61616 -p 8161:8161 --name activemq --net-alias activemq --net=uvms -v c:/uvms/activemq:/opt/jboss/activemq/data -d uvms/activemq:5.13.2
 docker run -it -p 5433:5432 --name postgres --net-alias postgres --net=uvms -d uvms/postgres-full
 timeout /t 60 /nobreak
 docker run -it -p 9990:9990 -p 8787:8787 -p 8080:8080 --name wildfly --net-alias wildfly --net=uvms -m 6G -d -v c:/uvms/app/logs:/app/logs -v c:/uvms/wildfly:/opt/jboss/wildfly/standalone/log uvms/wildfly-full
