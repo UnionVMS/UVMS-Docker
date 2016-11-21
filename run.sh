c7e1a5cc94e9d1aa1c426d2f1b755172e3f9c337
Oscar Djupfeldt <oscar.djupfeldt@havochvatten.se>
2016-11-21 13:58:25 +0100

    Increased delay

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ docker network rm uvms
 docker network create uvms
 docker run -it -p 61616:61616 -p 8161:8161 --name activemq --net-alias activemq --net=uvms -d uvms/activemq:5.13.2
 docker run -it -p 5433:5432 --name postgres --net-alias postgres --net=uvms -d uvms/postgres-full
-sleep 60
+sleep 180
 docker run -it -p 9990:9990 -p 8787:8787 -p 8080:8080 --name wildfly --net-alias wildfly --net=uvms -m 6G -d uvms/wildfly-full
