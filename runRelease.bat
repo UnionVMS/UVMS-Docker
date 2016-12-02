c1853e7a0186a6b9f6778b548ec6db236c3e72cb
Oscar Djupfeldt <oscar.djupfeldt@havochvatten.se>
2016-12-02 11:16:44 +0100

    Updated postgres dockerfile to match new dev branch for Audit Added runRelease.bat/sh which downloads and runs the latest release version.

---
+++ runRelease.bat
@@ -0,0 +1,6 @@
+
+docker network create uvms
+docker run -it -p 61616:61616 -p 8161:8161 --name activemq --net-alias activemq --net=uvms -v c:/uvms/activemq:/opt/jboss/activemq/data -d uvms/activemq:5.13.2
+docker run -it -p 5433:5432 --name postgres --net-alias postgres --net=uvms -d uvms/postgres-release
+timeout /t 180 /nobreak
+docker run -it -p 9990:9990 -p 8787:8787 -p 8080:8080 --name wildfly --net-alias wildfly --net=uvms -m 6G -d -v c:/uvms/app/logs:/app/logs -v c:/uvms/wildfly:/opt/jboss/wildfly/standalone/log uvms/wildfly-release
