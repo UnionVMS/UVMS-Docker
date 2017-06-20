87579cc5e5dc660b8a1df0662d901ffe240ddf64
pether.sorling@gmail.com <pether.sorling@gmail.com>
2017-06-20 18:51:39 +0200

    use released versions of mobile and movement

--- wildfly-release/jboss-deployment/add-jboss-all-to-ear.sh
+++ wildfly-release/jboss-deployment/add-jboss-all-to-ear.sh
@@ -31,13 +31,13 @@ addJbossAll config-dbaccess-module-3.0.4 /opt/jboss/jboss-deployment/config-db/j
 addJbossAll config-module-3.0.5 /opt/jboss/jboss-deployment/config/jboss-all.xml
 addJbossAll spatial-module-postgres-0.5.6.presweden2 /opt/jboss/jboss-deployment/spatial/jboss-all.xml
 addJbossAll reporting-module-0.5.7.presweden1 /opt/jboss/jboss-deployment/reporting/jboss-all.xml
-addJbossAll movement-module-3.0.9-SNAPSHOT /opt/jboss/jboss-deployment/movement/jboss-all.xml
+addJbossAll movement-module-3.0.8 /opt/jboss/jboss-deployment/movement/jboss-all.xml
 addJbossAll asset-module-3.0.8-SNAPSHOT /opt/jboss/jboss-deployment/asset/jboss-all.xml
 addJbossAll audit-dbaccess-module-3.0.4 /opt/jboss/jboss-deployment/general/jboss-all.xml
 addJbossAll audit-module-3.0.5 /opt/jboss/jboss-deployment/audit/jboss-all.xml
 addJbossAll exchange-dbaccess-module-3.0.4 /opt/jboss/jboss-deployment/general/jboss-all.xml
 addJbossAll exchange-module-3.0.5 /opt/jboss/jboss-deployment/exchange/jboss-all.xml
-addJbossAll mobileterminal-module-3.0.9-SNAPSHOT /opt/jboss/jboss-deployment/mobile/jboss-all.xml
+addJbossAll mobileterminal-module-3.0.8 /opt/jboss/jboss-deployment/mobile/jboss-all.xml
 addJbossAll rules-dbaccess-module-3.0.3 /opt/jboss/jboss-deployment/general/jboss-all.xml
 addJbossAll rules-module-3.0.1 /opt/jboss/jboss-deployment/rules/jboss-all.xml
 addJbossAll naf-module-3.0.0 /opt/jboss/jboss-deployment/plugins/jboss-all.xml
