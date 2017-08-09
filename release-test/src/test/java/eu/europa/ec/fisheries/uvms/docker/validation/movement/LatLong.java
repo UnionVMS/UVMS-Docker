56ebb8180677a9c29f0881c8b529920901c8636a
thomasfanto <thomas.fanto@consid.se>
2017-08-09 14:34:16 +0200

    rutt support-test

---
+++ release-test/src/test/java/eu/europa/ec/fisheries/uvms/docker/validation/movement/LatLong.java
@@ -0,0 +1,13 @@
+package eu.europa.ec.fisheries.uvms.docker.validation.movement;
+
+public class LatLong {
+	
+	public double latitude;
+	public double longitude;
+	public LatLong(double latitude, double longitude) {
+		super();
+		this.latitude = latitude;
+		this.longitude = longitude;
+	}
+
+}
