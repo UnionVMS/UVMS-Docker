e7659b39b8ac8f1ff275c2d23bdccd8af22244bd
James Sörling <james.p.sorling@consid.se>
2017-08-10 14:38:23 +0200

    test skeletons.

---
+++ release-test/src/test/java/eu/europa/ec/fisheries/uvms/docker/validation/rules/CustomRulesRestIT.java
@@ -0,0 +1,155 @@
+/*
+
+Developed with the contribution of the European Commission - Directorate General for Maritime Affairs and Fisheries
+� European Union, 2017.
+
+This file is part of the Integrated Fisheries Data Management (IFDM) Suite. The IFDM Suite is free software: you can
+redistribute it and/or modify it under the terms of the GNU General Public License as published by the
+Free Software Foundation, either version 3 of the License, or any later version. The IFDM Suite is distributed in
+the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should have received a
+copy of the GNU General Public License along with the IFDM Suite. If not, see <http://www.gnu.org/licenses/>.
+
+*/
+package eu.europa.ec.fisheries.uvms.docker.validation.rules;
+
+import java.util.Map;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.fluent.Request;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import eu.europa.ec.fisheries.schema.rules.customrule.v1.CustomRuleType;
+import eu.europa.ec.fisheries.schema.rules.customrule.v1.UpdateSubscriptionType;
+import eu.europa.ec.fisheries.schema.rules.search.v1.CustomRuleQuery;
+import eu.europa.ec.fisheries.uvms.docker.validation.common.AbstractRestServiceTest;
+import un.unece.uncefact.data.standard.fluxresponsemessage._6.FLUXResponseMessage;
+
+/**
+ * The Class CustomRulesRestIT.
+ */
+public class CustomRulesRestIT extends AbstractRestServiceTest {
+
+	/**
+	 * Creates the custom rule test.
+	 *
+	 * @throws Exception the exception
+	 */
+	@Test
+	@Ignore
+	public void createCustomRuleTest() throws Exception {
+		CustomRuleType customRuleType = new CustomRuleType();
+		final HttpResponse response = Request.Post(getBaseUrl() + "rules/rest/customrules/")
+				.setHeader("Content-Type", "application/json").setHeader("Authorization", getValidJwtToken())
+				.bodyByteArray(writeValueAsString(customRuleType).getBytes()).execute().returnResponse();
+		Map<String, Object> dataMap = checkSuccessResponseReturnMap(response);
+	}
+
+	/**
+	 * Gets the custom rules by user.
+	 *
+	 * @return the custom rules by user
+	 * @throws Exception the exception
+	 */
+	@Test
+	@Ignore
+	public void getCustomRulesByUser() throws Exception {
+		CustomRuleQuery CustomRuleQuery = new CustomRuleQuery();
+		final HttpResponse response = Request.Get(getBaseUrl() + "rules/rest/customrules/listAll/{userName}")
+				.setHeader("Content-Type", "application/json").setHeader("Authorization", getValidJwtToken()).execute()
+				.returnResponse();
+		Map<String, Object> dataMap = checkSuccessResponseReturnMap(response);
+	}
+
+	/**
+	 * Gets the custom rules by query test.
+	 *
+	 * @return the custom rules by query test
+	 * @throws Exception the exception
+	 */
+	@Test
+	@Ignore
+	public void getCustomRulesByQueryTest() throws Exception {
+		CustomRuleQuery CustomRuleQuery = new CustomRuleQuery();
+		final HttpResponse response = Request.Post(getBaseUrl() + "rules/rest/customrules/listByQuery")
+				.setHeader("Content-Type", "application/json").setHeader("Authorization", getValidJwtToken())
+				.bodyByteArray(writeValueAsString(CustomRuleQuery).getBytes()).execute().returnResponse();
+		Map<String, Object> dataMap = checkSuccessResponseReturnMap(response);
+	}
+
+	/**
+	 * Gets the custom rule by guid test.
+	 *
+	 * @return the custom rule by guid test
+	 * @throws Exception the exception
+	 */
+	@Test
+	@Ignore
+	public void getCustomRuleByGuidTest() throws Exception {
+		final HttpResponse response = Request.Get(getBaseUrl() + "rules/rest/customrules/{guid}")
+				.setHeader("Content-Type", "application/json").setHeader("Authorization", getValidJwtToken()).execute()
+				.returnResponse();
+		Map<String, Object> dataMap = checkSuccessResponseReturnMap(response);
+	}
+
+	/**
+	 * Delete custom rule test.
+	 *
+	 * @throws Exception the exception
+	 */
+	@Test
+	@Ignore
+	public void deleteCustomRuleTest() throws Exception {
+		final HttpResponse response = Request.Delete(getBaseUrl() + "rules/rest/customrules/{guid}")
+				.setHeader("Content-Type", "application/json").setHeader("Authorization", getValidJwtToken()).execute()
+				.returnResponse();
+		Map<String, Object> dataMap = checkSuccessResponseReturnMap(response);
+	}
+
+	/**
+	 * Update subscription test.
+	 *
+	 * @throws Exception the exception
+	 */
+	@Test
+	@Ignore
+	public void updateSubscriptionTest() throws Exception {
+		UpdateSubscriptionType updateSubscriptionType = new UpdateSubscriptionType();
+		final HttpResponse response = Request.Post(getBaseUrl() + "rules/rest/customrules/subscription")
+				.setHeader("Content-Type", "application/json").setHeader("Authorization", getValidJwtToken())
+				.bodyByteArray(writeValueAsString(updateSubscriptionType).getBytes()).execute().returnResponse();
+		Map<String, Object> dataMap = checkSuccessResponseReturnMap(response);
+	}
+
+	/**
+	 * Update test.
+	 *
+	 * @throws Exception the exception
+	 */
+	@Test
+	@Ignore
+	public void updateTest() throws Exception {
+		CustomRuleType customRuleType = new CustomRuleType();
+		final HttpResponse response = Request.Put(getBaseUrl() + "rules/rest/customrules/")
+				.setHeader("Content-Type", "application/json").setHeader("Authorization", getValidJwtToken())
+				.bodyByteArray(writeValueAsString(customRuleType).getBytes()).execute().returnResponse();
+		Map<String, Object> dataMap = checkSuccessResponseReturnMap(response);
+	}
+
+	/**
+	 * Evaluate FLUX response message test.
+	 *
+	 * @throws Exception the exception
+	 */
+	@Test
+	@Ignore
+	public void evaluateFLUXResponseMessageTest() throws Exception {
+		FLUXResponseMessage fluxResponseMessage = new FLUXResponseMessage();
+		final HttpResponse response = Request.Post(getBaseUrl() + "rules/rest/rules/evaluate/fluxfaResponsemessage")
+				.setHeader("Content-Type", "application/json").setHeader("Authorization", getValidJwtToken())
+				.bodyByteArray(writeValueAsString(fluxResponseMessage).getBytes()).execute().returnResponse();
+		Map<String, Object> dataMap = checkSuccessResponseReturnMap(response);
+	}
+
+}
---
+++ release-test/src/test/java/eu/europa/ec/fisheries/uvms/docker/validation/rules/RulesRestIT.java
@@ -0,0 +1,94 @@
+/*
+
+Developed with the contribution of the European Commission - Directorate General for Maritime Affairs and Fisheries
+� European Union, 2017.
+
+This file is part of the Integrated Fisheries Data Management (IFDM) Suite. The IFDM Suite is free software: you can
+redistribute it and/or modify it under the terms of the GNU General Public License as published by the
+Free Software Foundation, either version 3 of the License, or any later version. The IFDM Suite is distributed in
+the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should have received a
+copy of the GNU General Public License along with the IFDM Suite. If not, see <http://www.gnu.org/licenses/>.
+
+*/
+package eu.europa.ec.fisheries.uvms.docker.validation.rules;
+
+import java.util.Map;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.fluent.Request;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import eu.europa.ec.fisheries.uvms.docker.validation.common.AbstractRestServiceTest;
+import un.unece.uncefact.data.standard.fluxfaquerymessage._3.FLUXFAQueryMessage;
+import un.unece.uncefact.data.standard.fluxfareportmessage._3.FLUXFAReportMessage;
+import un.unece.uncefact.data.standard.fluxresponsemessage._6.FLUXResponseMessage;
+
+/**
+ * The Class RulesRestIT.
+ */
+
+public class RulesRestIT extends AbstractRestServiceTest {
+
+	/**
+	 * Initialize rules test.
+	 *
+	 * @throws Exception the exception
+	 */
+	@Test
+	public void initializeRulesTest() throws Exception {
+		final HttpResponse response = Request.Get(getBaseUrl() + "rules/rest/rules/reinitialize")
+				.setHeader("Content-Type", "application/json").setHeader("Authorization", getValidJwtToken()).execute()
+				.returnResponse();
+		String responseMessage = IOUtils.toString(response.getEntity().getContent(), "UTF-8");
+		assertEquals("Initialization successfully finished. The Rules DRLs were reloaded.", responseMessage);
+	}
+
+	/**
+	 * Evaluate FLUXFA report message test.
+	 *
+	 * @throws Exception the exception
+	 */
+	@Test
+	@Ignore
+	public void evaluateFLUXFAReportMessageTest() throws Exception {
+		FLUXFAReportMessage fluxfaReportMessage = new FLUXFAReportMessage();
+		final HttpResponse response = Request.Post(getBaseUrl() + "rules/rest/rules/evaluate/fluxfareportmessage")
+				.setHeader("Content-Type", "application/json").setHeader("Authorization", getValidJwtToken())
+				.bodyByteArray(writeValueAsString(fluxfaReportMessage).getBytes()).execute().returnResponse();
+		Map<String, Object> dataMap = checkSuccessResponseReturnMap(response);
+	}
+
+	/**
+	 * Evaluate FLUXFA query message message test.
+	 *
+	 * @throws Exception the exception
+	 */
+	@Test
+	@Ignore
+	public void evaluateFLUXFAQueryMessageMessageTest() throws Exception {
+		FLUXFAQueryMessage fluxfaQueryMessage = new FLUXFAQueryMessage();
+		final HttpResponse response = Request.Post(getBaseUrl() + "rules/rest/rules/evaluate/fluxfaquerymessage")
+				.setHeader("Content-Type", "application/json").setHeader("Authorization", getValidJwtToken())
+				.bodyByteArray(writeValueAsString(fluxfaQueryMessage).getBytes()).execute().returnResponse();
+		Map<String, Object> dataMap = checkSuccessResponseReturnMap(response);
+	}
+
+	/**
+	 * Evaluate FLUX response message test.
+	 *
+	 * @throws Exception the exception
+	 */
+	@Test
+	@Ignore
+	public void evaluateFLUXResponseMessageTest() throws Exception {
+		FLUXResponseMessage fluxResponseMessage = new FLUXResponseMessage();
+		final HttpResponse response = Request.Post(getBaseUrl() + "rules/rest/rules/evaluate/fluxfaResponsemessage")
+				.setHeader("Content-Type", "application/json").setHeader("Authorization", getValidJwtToken())
+				.bodyByteArray(writeValueAsString(fluxResponseMessage).getBytes()).execute().returnResponse();
+		Map<String, Object> dataMap = checkSuccessResponseReturnMap(response);
+	}
+
+}
