cecce512ddbb3a0575a50474c9985c3c7998d343
James Sörling <james.p.sorling@consid.se>
2017-08-11 14:56:30 +0200

    update spatial-app 0.5.8 -> 0.5.9

--- release-test/src/test/java/eu/europa/ec/fisheries/uvms/docker/validation/spatial/SpatialHelper.java
+++ release-test/src/test/java/eu/europa/ec/fisheries/uvms/docker/validation/spatial/SpatialHelper.java
@@ -2,6 +2,9 @@ package eu.europa.ec.fisheries.uvms.docker.validation.spatial;
 
 import java.io.StringReader;
 import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 import javax.jms.Message;
 import javax.jms.TextMessage;
@@ -10,6 +13,7 @@ import javax.xml.bind.JAXBException;
 
 import eu.europa.ec.fisheries.uvms.docker.validation.common.AbstractHelper;
 import eu.europa.ec.fisheries.uvms.docker.validation.common.MessageHelper;
+import eu.europa.ec.fisheries.uvms.docker.validation.movement.LatLong;
 import eu.europa.ec.fisheries.uvms.spatial.model.schemas.SpatialEnrichmentRQ;
 import eu.europa.ec.fisheries.uvms.spatial.model.schemas.SpatialEnrichmentRS;
 
@@ -38,4 +42,28 @@ public class SpatialHelper extends AbstractHelper {
 		Message messageResponse = MessageHelper.getMessageResponse(UVMS_SPATIAL_REQUEST_QUEUE, marshall(request));
 		return unMarshall(messageResponse);
 	}
+	
+	public List<LatLong> createRutt(int numberPositions) {
+		return createRutt(15 * 1000,numberPositions);
+	}
+
+	public List<LatLong> createRutt(int movementTimeDeltaInMillis, int numberPositions) {
+
+		List<LatLong> rutt = new ArrayList<>();
+		long ts = System.currentTimeMillis();
+		rutt.add(new LatLong(50d, 50d, getDate(ts += movementTimeDeltaInMillis)));
+		rutt.add(new LatLong(49d, 49d, getDate(ts += movementTimeDeltaInMillis)));
+		rutt.add(new LatLong(48d, 48d, getDate(ts += movementTimeDeltaInMillis)));
+		rutt.add(new LatLong(47d, 47d, getDate(ts += movementTimeDeltaInMillis)));
+		rutt.add(new LatLong(46d, 46d, getDate(ts += movementTimeDeltaInMillis)));
+		rutt.add(new LatLong(45d, 45d, getDate(ts += movementTimeDeltaInMillis)));
+		
+		return rutt.subList(0, numberPositions);
+
+	}
+
+	private Date getDate(Long millis) {
+		return new Date(millis);
+	}
+
 }
