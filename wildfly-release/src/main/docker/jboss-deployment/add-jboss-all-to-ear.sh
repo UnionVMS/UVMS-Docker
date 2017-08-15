11bfac575851c9511a1bebd65dcb74ecebb3429b
James Sörling <james.p.sorling@consid.se>
2017-08-15 13:30:10 +0200

    update exchange 3.0.9, with added metrics.

--- wildfly-release/src/main/docker/jboss-deployment/add-jboss-all-to-ear.sh
+++ wildfly-release/src/main/docker/jboss-deployment/add-jboss-all-to-ear.sh
@@ -36,7 +36,7 @@ addJbossAll asset-module-3.0.11 /opt/jboss/jboss-deployment/asset/jboss-all.xml
 addJbossAll audit-dbaccess-module-3.0.4 /opt/jboss/jboss-deployment/general/jboss-all.xml
 addJbossAll audit-module-3.0.8 /opt/jboss/jboss-deployment/audit/jboss-all.xml
 addJbossAll exchange-dbaccess-module-3.0.5 /opt/jboss/jboss-deployment/general/jboss-all.xml
-addJbossAll exchange-module-3.0.8 /opt/jboss/jboss-deployment/exchange/jboss-all.xml
+addJbossAll exchange-module-3.0.9 /opt/jboss/jboss-deployment/exchange/jboss-all.xml
 addJbossAll mobileterminal-module-3.0.11 /opt/jboss/jboss-deployment/mobile/jboss-all.xml
 addJbossAll rules-dbaccess-module-3.0.4 /opt/jboss/jboss-deployment/general/jboss-all.xml
 addJbossAll rules-module-3.0.3 /opt/jboss/jboss-deployment/rules/jboss-all.xml
