69b4280de44bd72cefef8ffd89ee357bd8e2eb81
James Sörling <james.p.sorling@consid.se>
2017-08-21 12:46:31 +0200

    fix incorrect version numbers for rules-module and rules-db-access-module.

--- wildfly-release/src/main/docker/jboss-deployment/add-jboss-all-to-ear.sh
+++ wildfly-release/src/main/docker/jboss-deployment/add-jboss-all-to-ear.sh
@@ -38,8 +38,8 @@ addJbossAll audit-module-3.0.9 /opt/jboss/jboss-deployment/audit/jboss-all.xml
 addJbossAll exchange-dbaccess-module-3.0.5 /opt/jboss/jboss-deployment/general/jboss-all.xml
 addJbossAll exchange-module-3.0.10 /opt/jboss/jboss-deployment/exchange/jboss-all.xml
 addJbossAll mobileterminal-module-3.0.11 /opt/jboss/jboss-deployment/mobile/jboss-all.xml
-addJbossAll rules-dbaccess-module-3.0.6 /opt/jboss/jboss-deployment/general/jboss-all.xml
-addJbossAll rules-module-3.0.5 /opt/jboss/jboss-deployment/rules/jboss-all.xml
+addJbossAll rules-dbaccess-module-3.0.5 /opt/jboss/jboss-deployment/general/jboss-all.xml
+addJbossAll rules-module-3.0.6 /opt/jboss/jboss-deployment/rules/jboss-all.xml
 addJbossAll activity-module-postgres-0.5.10 /opt/jboss/jboss-deployment/activity/jboss-all.xml
 addJbossAll mdr-module-0.5.0 /opt/jboss/jboss-deployment/mdr/jboss-all.xml
 addJbossAll naf-module-3.0.0 /opt/jboss/jboss-deployment/plugins/jboss-all.xml
