27e2769e9733510a4fa575c1e1268d2db0c5e7e8
thomasfanto <thomas.fanto@consid.se>
2017-09-29 09:22:39 +0200

    fixed the test so equals to thatone in movement

--- release-test/src/test/java/eu/europa/ec/fisheries/uvms/docker/validation/movement/MovementHelper.java
+++ release-test/src/test/java/eu/europa/ec/fisheries/uvms/docker/validation/movement/MovementHelper.java
@@ -445,28 +445,6 @@ public class MovementHelper extends AbstractHelper {
 	}
 
 
-
-	private double bearingInDegreesTEST(LatLong src, LatLong dst) {
-		return bearing(src.latitude, src.longitude, dst.latitude, dst.longitude);
-	}
-
-
-
-	protected static double bearing(double lat1, double lon1, double lat2, double lon2) {
-		double longitude1 = lon1;
-		double longitude2 = lon2;
-		double latitude1 = Math.toRadians(lat1);
-		double latitude2 = Math.toRadians(lat2);
-		double longDiff = Math.toRadians(longitude2 - longitude1);
-		double y = Math.sin(longDiff) * Math.cos(latitude2);
-		double x = Math.cos(latitude1) * Math.sin(latitude2)
-				- Math.sin(latitude1) * Math.cos(latitude2) * Math.cos(longDiff);
-
-		return (Math.toDegrees(Math.atan2(y, x)) + 360) % 360;
-	}
-
-
-
 	private double bearingInRadians(LatLong src, LatLong dst) {
 		double srcLat = Math.toRadians(src.latitude);
 		double dstLat = Math.toRadians(dst.latitude);
