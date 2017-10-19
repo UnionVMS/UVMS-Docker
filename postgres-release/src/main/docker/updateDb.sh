446cfe39382b64f10bc16b57b5d28227ccceb7e7
James Sörling <james.p.sorling@consid.se>
2017-10-19 14:14:00 +0200

    disable psql output

--- postgres-release/src/main/docker/updateDb.sh
+++ postgres-release/src/main/docker/updateDb.sh
@@ -14,19 +14,28 @@ psql -U postgres postgres -c "SELECT pg_reload_conf()"
 
 
 echo "Running module.sql to create tables and init data"
-psql -U asset -d db71u -q -f  /var/lib/postgresql/eu.europa.ec.fisheries.uvms.asset.liquibase-${unionvms.project.asset.module}.sql
-psql -U audit -d db71u -q -f  /var/lib/postgresql/eu.europa.ec.fisheries.uvms.audit.liquibase-${unionvms.project.audit.module}.sql
-psql -U config -d db71u -q -f  /var/lib/postgresql/eu.europa.ec.fisheries.uvms.config.liquibase-${unionvms.project.config.module}.sql
-psql -U exchange -d db71u -q -f  /var/lib/postgresql/eu.europa.ec.fisheries.uvms.exchange.liquibase-${unionvms.project.exchange.module}.sql
-psql -U mobterm -d db71u -q -f  /var/lib/postgresql/eu.europa.ec.fisheries.uvms.mobileterminal.liquibase-${unionvms.project.mobileterminal.module}.sql
-psql -U movement -d db71u -q -f  /var/lib/postgresql/eu.europa.ec.fisheries.uvms.movement.liquibase-${unionvms.project.movement.module}.sql
-psql -U rules -d db71u -a -q -f  /var/lib/postgresql/eu.europa.ec.fisheries.uvms.rules.liquibase-${unionvms.project.rules.module}.sql
-
-psql -U usm -d db71u -q -f /var/lib/postgresql/eu.europa.ec.fisheries.uvms.user.liquibase-2.0.3-SNAPSHOT.sql
-psql -U spatial -d db71u -q -f /var/lib/postgresql/eu.europa.ec.fisheries.uvms.spatial.liquibase-1.0.5-SNAPSHOT.sql
-psql -U mdr -d db71u -q  -a -f /var/lib/postgresql/eu.europa.ec.fisheries.uvms.mdr.liquibase-1.0.2-SNAPSHOT.sql
-psql -U activity -d db71u -q -f /var/lib/postgresql/eu.europa.ec.fisheries.uvms.activity.liquibase-1.0.1-SNAPSHOT.sql
-
+echo "Running asset.sql to create tables and init data"
+psql -U asset -d db71u -q -f  /var/lib/postgresql/eu.europa.ec.fisheries.uvms.asset.liquibase-${unionvms.project.asset.module}.sql >/dev/null
+echo "Running audit.sql to create tables and init data"
+psql -U audit -d db71u -q -f  /var/lib/postgresql/eu.europa.ec.fisheries.uvms.audit.liquibase-${unionvms.project.audit.module}.sql >/dev/null
+echo "Running config.sql to create tables and init data"
+psql -U config -d db71u -q -f  /var/lib/postgresql/eu.europa.ec.fisheries.uvms.config.liquibase-${unionvms.project.config.module}.sql >/dev/null
+echo "Running exchange.sql to create tables and init data"
+psql -U exchange -d db71u -q -f  /var/lib/postgresql/eu.europa.ec.fisheries.uvms.exchange.liquibase-${unionvms.project.exchange.module}.sql >/dev/null
+echo "Running mobterm.sql to create tables and init data"
+psql -U mobterm -d db71u -q -f  /var/lib/postgresql/eu.europa.ec.fisheries.uvms.mobileterminal.liquibase-${unionvms.project.mobileterminal.module}.sql >/dev/null
+echo "Running movement.sql to create tables and init data"
+psql -U movement -d db71u -q -f  /var/lib/postgresql/eu.europa.ec.fisheries.uvms.movement.liquibase-${unionvms.project.movement.module}.sql >/dev/null
+echo "Running rules.sql to create tables and init data"
+psql -U rules -d db71u -a -q -f  /var/lib/postgresql/eu.europa.ec.fisheries.uvms.rules.liquibase-${unionvms.project.rules.module}.sql >/dev/null
+echo "Running usm.sql to create tables and init data"
+psql -U usm -d db71u -q -f /var/lib/postgresql/eu.europa.ec.fisheries.uvms.user.liquibase-2.0.3-SNAPSHOT.sql >/dev/null
+echo "Running spatial.sql to create tables and init data"
+psql -U spatial -d db71u -q -f /var/lib/postgresql/eu.europa.ec.fisheries.uvms.spatial.liquibase-1.0.5-SNAPSHOT.sql >/dev/null
+echo "Running mdr.sql to create tables and init data"
+psql -U mdr -d db71u -q  -a -f /var/lib/postgresql/eu.europa.ec.fisheries.uvms.mdr.liquibase-1.0.2-SNAPSHOT.sql >/dev/null
+echo "Running activity.sql to create tables and init data"
+psql -U activity -d db71u -q -f /var/lib/postgresql/eu.europa.ec.fisheries.uvms.activity.liquibase-1.0.1-SNAPSHOT.sql >/dev/null
 echo "Completed module.sql"
 
 
