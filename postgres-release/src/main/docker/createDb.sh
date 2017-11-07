6cc85f29acc4d4469bd929c05ae1f77ec7857f87
James Sörling <james.p.sorling@consid.se>
2017-11-07 12:15:17 +0100

    prepare db setup outside of docker.

--- postgres-release/src/main/docker/createDb.sh
+++ postgres-release/src/main/docker/createDb.sh
@@ -1,4 +1,9 @@
-#!bin/sh
+#!/bin/sh
+
+if [ $# -ne 1 ]; 
+    then echo "Requires one argument postgresql-db-hostname"
+    exit 1
+fi
 
 echo "Running setup.sql"
-psql --single-transaction -q -f  setup.sql >/dev/null
+psql --host=$1 -U postgres  --single-transaction -q -f  setup.sql >/dev/null
--- postgres-release/src/main/docker/dropDb.sh
+++ postgres-release/src/main/docker/dropDb.sh
@@ -1,4 +1,9 @@
-#!bin/sh
+#!/bin/sh
+
+if [ $# -ne 1 ]; 
+    then echo "Requires one argument postgresql-db-hostname"
+    exit 1
+fi
 
 echo "Running drop-all.sql"
-psql --single-transaction -q -f  drop-all.sql >/dev/null
+psql --host=$1 -U postgres --single-transaction -q -f  drop-all.sql >/dev/null
