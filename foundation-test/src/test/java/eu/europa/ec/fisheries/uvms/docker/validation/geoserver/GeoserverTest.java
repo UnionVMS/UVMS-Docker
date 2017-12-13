c865fa7f52985843b57e0ea76bb34cf962b6226d
James Sörling <james.p.sorling@consid.se>
2017-12-13 12:18:05 +0100

    cors test

--- foundation-test/src/test/java/eu/europa/ec/fisheries/uvms/docker/validation/geoserver/GeoserverTest.java
+++ foundation-test/src/test/java/eu/europa/ec/fisheries/uvms/docker/validation/geoserver/GeoserverTest.java
@@ -34,6 +34,42 @@ public class GeoserverTest extends AbstractRestServiceTest {
 		assertEquals(HttpStatus.SC_OK, httpResponse.getStatusLine().getStatusCode());
 		
 		assertEquals("SAMEORIGIN", httpResponse.getFirstHeader("X-Frame-Options").getValue());
+
+		
+	}	
+	
+	/**
+	 * Verify access control allow origin test.
+	 *
+	 * @throws Exception the exception
+	 */
+	@Test
+	public void verifyAccessControlAllowOriginTest() throws Exception {
+		HttpResponse httpResponse = Request.Get("http://localhost:28080/geoserver/web/").setHeader("Authorization", getValidJwtToken()).setHeader("Origin", "http://www.example.com").execute().returnResponse();
+		assertEquals(HttpStatus.SC_OK, httpResponse.getStatusLine().getStatusCode());
+		
+		assertEquals("SAMEORIGIN", httpResponse.getFirstHeader("X-Frame-Options").getValue());
+		assertEquals("http://www.example.com", httpResponse.getFirstHeader("Access-Control-Allow-Origin").getValue());
+		assertEquals("true", httpResponse.getFirstHeader("Access-Control-Allow-Credentials").getValue());
+				
+	}
+
+	/**
+	 * Verify options allow test.
+	 *
+	 * @throws Exception the exception
+	 */
+	@Test
+	public void verifyOptionsAllowTest() throws Exception {
+		HttpResponse httpResponse = Request.Options("http://localhost:28080/geoserver/web/").setHeader("Authorization", getValidJwtToken()).setHeader("Origin", "http://www.example.com").setHeader("Access-Control-Request-Method","POST").setHeader("Access-Control-Request-Headers", "content-type,accept").execute().returnResponse();
+		assertEquals(HttpStatus.SC_OK, httpResponse.getStatusLine().getStatusCode());
+		
+		assertEquals("GET, HEAD, POST, PUT, DELETE, TRACE, OPTIONS, PATCH", httpResponse.getFirstHeader("Allow").getValue());
+		assertEquals("http://www.example.com", httpResponse.getFirstHeader("Access-Control-Allow-Origin").getValue());
+		assertEquals("true", httpResponse.getFirstHeader("Access-Control-Allow-Credentials").getValue());
+		assertEquals("GET,POST,HEAD",httpResponse.getFirstHeader("Access-Control-Allow-Methods").getValue());
+		assertEquals("X-Requested-With,Content-Type,Accept,Origin",httpResponse.getFirstHeader("Access-Control-Allow-Headers").getValue());
 						
 	}
+
 }
