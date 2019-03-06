f3e066d5314d4d8dda6330e98c60f6482411968f
peerik <peter.eriksson@havochvatten.se>
2019-03-06 11:56:16 +0100

    Remove hibernate wrapper

--- postgres-release/src/main/docker/updateDb.sh
+++ postgres-release/src/main/docker/updateDb.sh
@@ -31,7 +31,7 @@ psql -U movementrules -d db71u -a --single-transaction -q -f  /var/lib/postgresq
 echo "Running usm.sql to create tables and init data"
 psql -U usm -d db71u --single-transaction -q -f /var/lib/postgresql/eu.europa.ec.fisheries.uvms.user.liquibase-${unionvms.project.user.module}.sql >/dev/null
 echo "Running spatial.sql to create tables and init data"
-psql -U spatial -d db71u --single-transaction -q -f /var/lib/postgresql/eu.europa.ec.fisheries.uvms.spatial.liquibase-${unionvms.project.spatial.db.module}.sql >/dev/null
+psql -U spatial -d db71u --single-transaction -q -f /var/lib/postgresql/eu.europa.ec.fisheries.uvms.spatial.liquibase-${unionvms.project.spatial.module}.sql >/dev/null
 echo "Running mdr.sql to create tables and init data"
 psql -U mdr -d db71u --single-transaction -q -f /var/lib/postgresql/eu.europa.ec.fisheries.uvms.mdr.liquibase-${unionvms.project.mdr.module}.sql >/dev/null
 echo "Running activity.sql to create tables and init data"
