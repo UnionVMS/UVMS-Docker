a496410b034bded77ab7c7e85c17c359c5e0a1d7
peerik <peter.eriksson@havochvatten.se>
2019-03-07 15:08:54 +0100

    clean up

--- unionvms-test/src/test/java/eu/europa/ec/fisheries/uvms/docker/validation/spatial/dto/SystemSettingsDto.java
+++ unionvms-test/src/test/java/eu/europa/ec/fisheries/uvms/docker/validation/spatial/dto/SystemSettingsDto.java
@@ -10,50 +10,15 @@ details. You should have received a copy of the GNU General Public License along
  */
 package eu.europa.ec.fisheries.uvms.docker.validation.spatial.dto;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-/**
- * Created by padhyad on 11/25/2015.
- */
-@JsonInclude(JsonInclude.Include.NON_NULL)
 public class SystemSettingsDto {
 
-    @JsonProperty("geoserverUrl")
     private String geoserverUrl;
 
-    @JsonProperty("bingApiKey")
-    private String bingApiKey;
-
-    public SystemSettingsDto() {}
-
-    public SystemSettingsDto(String geoserverUrl, String bingApiKey) {
-        this.geoserverUrl = geoserverUrl;
-        this.bingApiKey = bingApiKey;
-    }
-
-    @JsonProperty("geoserverUrl")
     public String getGeoserverUrl() {
         return geoserverUrl;
     }
 
-    @JsonProperty("geoserverUrl")
     public void setGeoserverUrl(String geoserverUrl) {
         this.geoserverUrl = geoserverUrl;
-    }
-
-    @JsonProperty("bingApiKey")
-    public String getBingApiKey() {
-        return bingApiKey;
-    }
-
-    @JsonProperty("bingApiKey")
-    public void setBingApiKey(String bingApiKey) {
-        this.bingApiKey = bingApiKey;
-    }
-
-    @Override
-    public String toString() {
-        return "ClassPojo [geoserverUrl = " + geoserverUrl + "]";
     }
 }
