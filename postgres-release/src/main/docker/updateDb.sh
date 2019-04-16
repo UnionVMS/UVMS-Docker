a793062aa39a5e8d42af17233df1f095c6c7f4e6
peerik <peter.eriksson@havochvatten.se>
2019-04-16 12:40:43 +0200

    Updated uvms backend modules with versions running usm4uvms 4.0.16. Asset and Geoserver is excluded.

--- postgres-release/src/main/docker/updateDb.sh
+++ postgres-release/src/main/docker/updateDb.sh
@@ -37,7 +37,7 @@ psql -U mdr -d db71u --single-transaction -q -f /var/lib/postgresql/eu.europa.ec
 echo "Running activity.sql to create tables and init data"
 psql -U activity -d db71u --single-transaction -q -f /var/lib/postgresql/eu.europa.ec.fisheries.uvms.activity.liquibase-${unionvms.project.activity.module}.sql >/dev/null
 echo "Running reporting.sql to create tables and init data"
-psql -U reporting -d db71u --single-transaction -q -f /var/lib/postgresql/eu.europa.ec.fisheries.uvms.reporting.liquibase-${unionvms.project.reporting.db.module}.sql >/dev/null
+psql -U reporting -d db71u --single-transaction -q -f /var/lib/postgresql/eu.europa.ec.fisheries.uvms.reporting.reporting-liquibase-${unionvms.project.reporting.module}.sql >/dev/null
 echo "Running subscription.sql to create tables and init data"
 psql -U subscription -d db71u --single-transaction -q -f /var/lib/postgresql/eu.europa.ec.fisheries.uvms.subscription.liquibase-${unionvms.project.subscription.module}.sql >/dev/null
 echo "Completed module.sql"
--- postgres-release/src/main/docker/updateDbOnly.sh
+++ postgres-release/src/main/docker/updateDbOnly.sh
@@ -29,7 +29,7 @@ psql -U mdr -d $1 --host=$2 --single-transaction -q -f eu.europa.ec.fisheries.uv
 echo "Running activity.sql to create tables and init data"
 psql -U activity -d $1 --host=$2 --single-transaction -q -f eu.europa.ec.fisheries.uvms.activity.liquibase-${unionvms.project.activity.module}.sql >/dev/null
 echo "Running reporting.sql to create tables and init data"
-psql -U reporting -d $1 --host=$2 --single-transaction -q -f eu.europa.ec.fisheries.uvms.reporting.liquibase-${unionvms.project.reporting.db.module}.sql >/dev/null
+psql -U reporting -d $1 --host=$2 --single-transaction -q -f eu.europa.ec.fisheries.uvms.reporting.reporting-liquibase-${unionvms.project.reporting.module}.sql >/dev/null
 echo "Running subscription.sql to create tables and init data"
 psql -U subscription -d $1 --host=$2 --single-transaction -q -f eu.europa.ec.fisheries.uvms.subscription.liquibase-${unionvms.project.subscription.module}.sql >/dev/null
 echo "Completed module.sql"
