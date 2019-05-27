f8ddb947e9e0f6b8ac89debaf61cb4b46f6e678c
peerik <peter.eriksson@havochvatten.se>
2019-05-27 14:56:45 +0200

    wait for activity.databasechangeloglock

--- wildfly-base/src/main/docker/start.sh
+++ wildfly-base/src/main/docker/start.sh
@@ -11,7 +11,7 @@ do
    	  printf "Database available. Wildfly starting.\n"
       break
    fi
-   locked=`psql -t -A -h postgres -p 5432 -U postgres -d db71u -c "select locked from config.databasechangeloglock;" 2>/dev/null`
+   locked=`psql -t -A -h postgres -p 5432 -U postgres -d db71u -c "select locked from activity.databasechangeloglock;" 2>/dev/null`
    if [[ -z "${locked// }" ]];then
       locked="y"
    fi
