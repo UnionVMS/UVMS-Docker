df4503b6d2a778e22fd439351cf58cb6a51de099
peerik <peter.eriksson@havochvatten.se>
2019-06-25 09:12:30 +0200

    basic config syncronization tests

---
+++ unionvms-test/src/test/java/eu/europa/ec/fisheries/uvms/docker/validation/system/ConfigSynchronizationIT.java
@@ -0,0 +1,82 @@
+/*
+﻿Developed with the contribution of the European Commission - Directorate General for Maritime Affairs and Fisheries
+© European Union, 2015-2016.
+
+This file is part of the Integrated Fisheries Data Management (IFDM) Suite. The IFDM Suite is free software: you can
+redistribute it and/or modify it under the terms of the GNU General Public License as published by the
+Free Software Foundation, either version 3 of the License, or any later version. The IFDM Suite is distributed in
+the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should have received a
+copy of the GNU General Public License along with the IFDM Suite. If not, see <http://www.gnu.org/licenses/>.
+ */
+package eu.europa.ec.fisheries.uvms.docker.validation.system;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import java.util.List;
+import java.util.Map;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
+import org.junit.Test;
+import eu.europa.ec.fisheries.schema.config.types.v1.SettingType;
+import eu.europa.ec.fisheries.schema.exchange.plugin.v1.SetConfigRequest;
+import eu.europa.ec.fisheries.uvms.commons.rest.dto.ResponseDto;
+import eu.europa.ec.fisheries.uvms.docker.validation.common.AbstractRest;
+import eu.europa.ec.fisheries.uvms.docker.validation.common.TopicListener;
+import eu.europa.ec.fisheries.uvms.docker.validation.system.helper.VMSSystemHelper;
+
+public class ConfigSynchronizationIT extends AbstractRest {
+
+    private static final String EXPECTED_GLOBAL_CONFIG = "flux_local_nation_code";
+    
+    @Test
+    public void checkAssetConfigTest() {
+        Map<String, String> response = getWebTarget()
+            .path("asset/rest/config/parameters")
+            .request(MediaType.APPLICATION_JSON)
+            .header(HttpHeaders.AUTHORIZATION, getValidJwtToken())
+            .get(new GenericType<Map<String, String>>() {});
+        
+        assertTrue(response.keySet().contains(EXPECTED_GLOBAL_CONFIG));
+    }
+    
+    @Test
+    public void nafPluginConfigUpdateTest() throws Exception {
+        String expectedKey = "eu.europa.ec.fisheries.uvms.plugins.naf.connectTimeout";
+        
+        ResponseDto<List<SettingType>> response = getWebTarget()
+                .path("config/rest/settings")
+                .queryParam("moduleName", "exchange")
+                .request(MediaType.APPLICATION_JSON)
+                .header(HttpHeaders.AUTHORIZATION, getValidJwtToken())
+                .get(new GenericType<ResponseDto<List<SettingType>>>() {});
+
+        SettingType expectedSetting = null;
+        for (SettingType settingType : response.getData()) {
+            if (settingType.getKey().equals(expectedKey)) {
+                expectedSetting = settingType;
+            }
+        }
+        assertThat(expectedSetting, is(notNullValue()));
+
+        String newValue = generateARandomStringWithMaxLength(6);
+        expectedSetting.setValue(newValue);
+        
+        try (TopicListener listener = new TopicListener(VMSSystemHelper.NAF_SELECTOR)) {
+            getWebTarget()
+                    .path("config/rest/settings")
+                    .path(expectedSetting.getId().toString())
+                    .request(MediaType.APPLICATION_JSON)
+                    .header(HttpHeaders.AUTHORIZATION, getValidJwtToken())
+                    .put(Entity.json(expectedSetting));
+
+            SetConfigRequest configRequest = listener.listenOnEventBusForSpecificMessage(SetConfigRequest.class);
+            assertThat(configRequest, is(notNullValue()));
+            assertThat(configRequest.getConfigurations().getSetting().size(), is(1));
+            assertThat(configRequest.getConfigurations().getSetting().get(0).getKey(), is(expectedKey));
+            assertThat(configRequest.getConfigurations().getSetting().get(0).getValue(), is(newValue));
+        }
+    }
+}
