7cd8c2861f9f4a8d47cc9aa9c075ff48eef6bf23
Jonas E.Mattsson <jonas.mattsson@consid.se>
2019-09-25 13:57:27 +0200

    updated another test

--- unionvms-test/src/test/java/eu/europa/ec/fisheries/uvms/docker/validation/system/vms/NAFSystemIT.java
+++ unionvms-test/src/test/java/eu/europa/ec/fisheries/uvms/docker/validation/system/vms/NAFSystemIT.java
@@ -112,21 +112,28 @@ public class NAFSystemIT extends AbstractRest {
         channel.setEndpointId(createdEndpoint.getEndpointId());
         UserHelper.createChannel(channel);
 
-        AssetDTO asset = AssetTestHelper.createTestAsset();
-        NAFHelper.sendPositionToNAFPlugin(new LatLong(58.973, 5.781, Date.from(Instant.now().minusMillis(10 * 60 * 1000))), asset);
-        MovementHelper.pollMovementCreated(); // First position for an asset creates ENT, ignore this
-        
         CustomRuleType flagStateRule = CustomRuleBuilder.getBuilder()
                 .setName("Area NOR => Send to NOR")
-                .rule(CriteriaType.AREA, SubCriteriaType.AREA_CODE, 
+                .rule(CriteriaType.AREA, SubCriteriaType.AREA_CODE,
                         ConditionType.EQ, "NOR")
                 .action(ActionType.SEND_REPORT, VMSSystemHelper.NAF_NAME, organisation.getName())
                 .build();
-        
+
         CustomRuleType createdCustomRule = CustomRuleHelper.createCustomRule(flagStateRule);
         assertNotNull(createdCustomRule);
+
+        AssetDTO asset = AssetTestHelper.createTestAsset();
+        LatLong position = new LatLong(58.973, 5.781, Date.from(Instant.now().minusMillis(10 * 60 * 1000)));
+        position.speed = 5;
+        NAFHelper.sendPositionToNAFPlugin(position, asset);
+        MovementHelper.pollMovementCreated(); // First position for an asset creates ENT, ignore this
+
+        try (NafEndpoint nafEndpoint = new NafEndpoint(ENDPOINT_PORT)) {
+            NAFHelper.sendPositionToNAFPlugin(position, asset);
+            nafEndpoint.getMessage(10000);
+        }
         
-        LatLong position = new LatLong(58.973, 5.781, Date.from(Instant.now()));
+        position = new LatLong(58.973, 5.781, Date.from(Instant.now()));
         position.speed = 5;
         
         String message;
