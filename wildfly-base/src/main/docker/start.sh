68b9ec2f6ac7d75bbeffea2cbc10e9769a46e167
psjobergprivat <psjobergprivat@gmail.com>
2019-09-27 16:18:21 +0200

    Modifying check of db started - now continues if any rows in activity.databasechangelog instead of looking at activity.databasechangeloglock since that table was never created when running liquibase with offline: db (this is not optimal either...)

--- wildfly-base/src/main/docker/start.sh
+++ wildfly-base/src/main/docker/start.sh
@@ -3,17 +3,17 @@
 rm -rf /app/logs/*
 rm -rf /opt/jboss/wildfly/standalone/log/*
 
-locked="y"
+empty="y"
 printf "Waiting for database.\n"
 while :
 do
-   if [[ ${locked} != *"y"* ]];then
+   if [[ ${empty} != *"y"* ]];then
    	  printf "Database available. Wildfly starting.\n"
       break
    fi
-   locked=`psql -t -A -h postgres -p 5432 -U postgres -d db71u -c "select locked from activity.databasechangeloglock;" 2>/dev/null`
-   if [[ -z "${locked// }" ]];then
-      locked="y"
+   empty=`psql -t -A -h postgres -p 5432 -U postgres -d db71u -c "select count(*) from activity.databasechangelog;" 2>/dev/null`
+   if [[ -z "${empty// }" ]];then
+      empty="y"
    fi
    sleep 5
 done
