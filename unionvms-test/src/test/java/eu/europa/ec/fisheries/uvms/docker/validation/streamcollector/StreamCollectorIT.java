479df99cef674d422dd49b9872f47409ee1726f6
Jonas E.Mattsson <jonas.mattsson@consid.se>
2019-11-01 11:56:52 +0100

    cleanup

--- unionvms-test/src/test/java/eu/europa/ec/fisheries/uvms/docker/validation/streamcollector/StreamCollectorIT.java
+++ unionvms-test/src/test/java/eu/europa/ec/fisheries/uvms/docker/validation/streamcollector/StreamCollectorIT.java
@@ -1,13 +1,9 @@
 package eu.europa.ec.fisheries.uvms.docker.validation.streamcollector;
 
-import eu.europa.ec.fisheries.uvms.asset.client.model.AssetDTO;
-import eu.europa.ec.fisheries.uvms.docker.validation.asset.AssetTestHelper;
 import eu.europa.ec.fisheries.uvms.docker.validation.common.AbstractRest;
 import eu.europa.ec.fisheries.uvms.docker.validation.common.MessageHelper;
 import eu.europa.ec.fisheries.uvms.docker.validation.movement.AuthorizationHeaderWebTarget;
-import eu.europa.ec.fisheries.uvms.docker.validation.movement.LatLong;
 import eu.europa.ec.fisheries.uvms.docker.validation.movement.MovementHelper;
-import eu.europa.ec.fisheries.uvms.docker.validation.movement.model.IncomingMovement;
 import eu.europa.ec.fisheries.uvms.docker.validation.rules.CustomRulesTestHelper;
 import org.hamcrest.CoreMatchers;
 import org.junit.AfterClass;
@@ -19,7 +15,6 @@ import javax.jms.JMSException;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.sse.SseEventSource;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 public class StreamCollectorIT extends AbstractRest {
@@ -50,7 +45,6 @@ public class StreamCollectorIT extends AbstractRest {
     @Test(timeout = 10000)
     public void sseTest() throws Exception {
         List<String> sseMessages = new ArrayList<>();
-        AssetDTO testAsset;
         String movementGuid;
 
         try (SseEventSource source = getSseStream()) {
@@ -59,16 +53,9 @@ public class StreamCollectorIT extends AbstractRest {
             });
             source.open();
 
-            //testAsset = AssetTestHelper.createTestAsset();
-            //LatLong position = new LatLong(11d, 12d, new Date());
 
             movementGuid = CustomRulesTestHelper.createRuleAndGetMovementGuid();
 
-
-          //  IncomingMovement incomingMovement = movementHelper.createIncomingMovement(testAsset, position);
-
-            //messageHelper.sendMessageWithFunction("UVMSMovementEvent", OBJECT_MAPPER.writeValueAsString(
-              //      incomingMovement), "CREATE");
 
             while(sseMessages.size() < 5) {         // 2 welcoming messages and then 1 for creating the asset, 1 for creating the movement and then lastly one for creating the ticket
                 Thread.sleep(100);
@@ -77,7 +64,6 @@ public class StreamCollectorIT extends AbstractRest {
         }
         assertThat(sseMessages.size(), CoreMatchers.is(5));
         assertTrue(sseMessages.stream().anyMatch(s -> s.contains("Updated Asset")));
-        //assertTrue(sseMessages.stream().anyMatch(s -> s.contains(testAsset.getId().toString())));
         assertTrue(sseMessages.stream().anyMatch(s -> s.contains("Movement") && s.contains(movementGuid)));
         assertTrue(sseMessages.stream().anyMatch(s -> s.contains("Ticket") && s.contains(movementGuid)));
     }
