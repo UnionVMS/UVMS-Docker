78fb0e8a4e4ab0a43a465dffaf61357eb23f7944
Ansgar Kroger <1104644+ansgarkroger@users.noreply.github.com>
2019-11-04 13:41:07 +0100

    depend on mare pom update versions

--- postgres-release/src/main/docker/updateDb.sh
+++ postgres-release/src/main/docker/updateDb.sh
@@ -16,33 +16,33 @@ sed -i -e"s/^#work_mem = 4MB.*$/work_mem = 16MB/" /var/lib/postgresql/data/postg
 echo "Running updateDb.sh to create tables and init data for all the projects.."
 
 echo "Running asset.sql to create tables and init data"
-psql -U asset -d db71u --single-transaction -q -f  /var/lib/postgresql/eu.europa.ec.fisheries.uvms.asset.liquibase-${unionvms.project.asset.module}.sql >/dev/null
+psql -U asset -d db71u --single-transaction -q -f  /var/lib/postgresql/eu.europa.ec.fisheries.uvms.asset.asset-liquibase-${unionvms.project.asset.module}.sql >/dev/null
 echo "Running audit.sql to create tables and init data"
-psql -U audit -d db71u --single-transaction -q -f  /var/lib/postgresql/eu.europa.ec.fisheries.uvms.audit.liquibase-${unionvms.project.audit.module}.sql >/dev/null
+psql -U audit -d db71u --single-transaction -q -f  /var/lib/postgresql/eu.europa.ec.fisheries.uvms.audit.audit-liquibase-${unionvms.project.audit.module}.sql >/dev/null
 echo "Running config.sql to create tables and init data"
-psql -U config -d db71u --single-transaction -q -f  /var/lib/postgresql/eu.europa.ec.fisheries.uvms.config.liquibase-${unionvms.project.config.module}.sql >/dev/null
+psql -U config -d db71u --single-transaction -q -f  /var/lib/postgresql/eu.europa.ec.fisheries.uvms.config.config-liquibase-${unionvms.project.config.module}.sql >/dev/null
 echo "Running exchange.sql to create tables and init data"
-psql -U exchange -d db71u --single-transaction -q -f  /var/lib/postgresql/eu.europa.ec.fisheries.uvms.exchange.liquibase-${unionvms.project.exchange.module}.sql >/dev/null
+psql -U exchange -d db71u --single-transaction -q -f  /var/lib/postgresql/eu.europa.ec.fisheries.uvms.exchange.exchange-liquibase-${unionvms.project.exchange.module}.sql >/dev/null
 echo "Running mobterm.sql to create tables and init data"
-psql -U mobterm -d db71u --single-transaction -q -f  /var/lib/postgresql/eu.europa.ec.fisheries.uvms.mobileterminal.liquibase-${unionvms.project.mobileterminal.module}.sql >/dev/null
+psql -U mobterm -d db71u --single-transaction -q -f  /var/lib/postgresql/eu.europa.ec.fisheries.uvms.mobileterminal.mobileterminal-liquibase-${unionvms.project.mobileterminal.module}.sql >/dev/null
 echo "Running movement.sql to create tables and init data"
-psql -U movement -d db71u --single-transaction -q -f  /var/lib/postgresql/eu.europa.ec.fisheries.uvms.movement.liquibase-${unionvms.project.movement.module}.sql >/dev/null
+psql -U movement -d db71u --single-transaction -q -f  /var/lib/postgresql/eu.europa.ec.fisheries.uvms.movement.movement-liquibase-${unionvms.project.movement.module}.sql >/dev/null
 echo "Running rules.sql to create tables and init data"
-psql -U rules -d db71u -a --single-transaction -q -f  /var/lib/postgresql/eu.europa.ec.fisheries.uvms.rules.liquibase-${unionvms.project.rules.module}.sql >/dev/null
+psql -U rules -d db71u -a --single-transaction -q -f  /var/lib/postgresql/eu.europa.ec.fisheries.uvms.rules.rules-liquibase-${unionvms.project.rules.module}.sql >/dev/null
 echo "Running movementrules.sql to create tables and init data"
 psql -U movementrules -d db71u -a --single-transaction -q -f  /var/lib/postgresql/eu.europa.ec.fisheries.uvms.movement-rules.liquibase-${unionvms.project.movementrules.module}.sql >/dev/null
 echo "Running usm.sql to create tables and init data"
-psql -U usm -d db71u --single-transaction -q -f /var/lib/postgresql/eu.europa.ec.fisheries.uvms.user.liquibase-${unionvms.project.user.liquibase.version}.sql >/dev/null
+psql -U usm -d db71u --single-transaction -q -f /var/lib/postgresql/eu.europa.ec.fisheries.uvms.user.user-liquibase-${unionvms.project.user.liquibase.version}.sql >/dev/null
 echo "Running spatial.sql to create tables and init data"
-psql -U spatial -d db71u --single-transaction -q -f /var/lib/postgresql/eu.europa.ec.fisheries.uvms.spatial.liquibase-${unionvms.project.spatial.module}.sql >/dev/null
+psql -U spatial -d db71u --single-transaction -q -f /var/lib/postgresql/eu.europa.ec.fisheries.uvms.spatial.spatial-liquibase-${unionvms.project.spatial.module}.sql >/dev/null
 echo "Running mdr.sql to create tables and init data"
-psql -U mdr -d db71u --single-transaction -q -f /var/lib/postgresql/eu.europa.ec.fisheries.uvms.mdr.liquibase-${unionvms.project.mdr.module}.sql >/dev/null
+psql -U mdr -d db71u --single-transaction -q -f /var/lib/postgresql/eu.europa.ec.fisheries.uvms.mdr.mdr-liquibase-${unionvms.project.mdr.module}.sql >/dev/null
 echo "Running activity.sql to create tables and init data"
-psql -U activity -d db71u --single-transaction -q -f /var/lib/postgresql/eu.europa.ec.fisheries.uvms.activity.liquibase-${unionvms.project.activity.module}.sql >/dev/null
+psql -U activity -d db71u --single-transaction -q -f /var/lib/postgresql/eu.europa.ec.fisheries.uvms.activity.activity-liquibase-${unionvms.project.activity.module}.sql >/dev/null
 echo "Running reporting.sql to create tables and init data"
-psql -U reporting -d db71u --single-transaction -q -f /var/lib/postgresql/eu.europa.ec.fisheries.uvms.reporting.liquibase-${unionvms.project.reporting.module}.sql >/dev/null
+psql -U reporting -d db71u --single-transaction -q -f /var/lib/postgresql/eu.europa.ec.fisheries.uvms.reporting.reporting-liquibase-${unionvms.project.reporting.module}.sql >/dev/null
 echo "Running subscription.sql to create tables and init data"
-psql -U subscription -d db71u --single-transaction -q -f /var/lib/postgresql/eu.europa.ec.fisheries.uvms.subscription.liquibase-${unionvms.project.subscription.module}.sql >/dev/null
+psql -U subscription -d db71u --single-transaction -q -f /var/lib/postgresql/eu.europa.ec.fisheries.uvms.subscription.subscription-liquibase-${unionvms.project.subscription.module}.sql >/dev/null
 
 echo "\n\nCompleted updateDb.sql"
 
--- postgres-release/src/main/docker/updateDbOnly.sh
+++ postgres-release/src/main/docker/updateDbOnly.sh
@@ -8,32 +8,32 @@ fi
 echo "Running module.sql to create tables and init data"
 
 echo "Running asset.sql to create tables and init data"
-psql -U asset -d $1 --host=$2 --single-transaction -q -f  eu.europa.ec.fisheries.uvms.asset.liquibase-${unionvms.project.asset.module}.sql >/dev/null
+psql -U asset -d $1 --host=$2 --single-transaction -q -f  eu.europa.ec.fisheries${unionvms.project.asset.module}.sql >/dev/null
 echo "Running  audit.sql to create tables and init data"
-psql -U audit -d $1 --host=$2 --single-transaction -q -f  eu.europa.ec.fisheries.uvms.audit.liquibase-${unionvms.project.audit.module}.sql >/dev/null
+psql -U audit -d $1 --host=$2 --single-transaction -q -f  eu.europa.ec.fisheries${unionvms.project.audit.module}.sql >/dev/null
 echo "Running config.sql to create tables and init data"
-psql -U config -d $1 --host=$2 --single-transaction -q -f  eu.europa.ec.fisheries.uvms.config.liquibase-${unionvms.project.config.module}.sql >/dev/null
+psql -U config -d $1 --host=$2 --single-transaction -q -f  eu.europa.ec.fisheries${unionvms.project.config.module}.sql >/dev/null
 echo "Running exchange.sql to create tables and init data"
-psql -U exchange -d $1 --host=$2 --single-transaction -q -f  eu.europa.ec.fisheries.uvms.exchange.liquibase-${unionvms.project.exchange.module}.sql >/dev/null
+psql -U exchange -d $1 --host=$2 --single-transaction -q -f  eu.europa.ec.fisheries${unionvms.project.exchange.module}.sql >/dev/null
 echo "Running mobterm.sql to create tables and init data"
-psql -U mobterm -d $1 --host=$2 --single-transaction -q -f  eu.europa.ec.fisheries.uvms.mobileterminal.liquibase-${unionvms.project.mobileterminal.module}.sql >/dev/null
+psql -U mobterm -d $1 --host=$2 --single-transaction -q -f  eu.europa.ec.fisheries${unionvms.project.mobileterminal.module}.sql >/dev/null
 echo "Running movement.sql to create tables and init data"
-psql -U movement -d $1 --host=$2 --single-transaction -q -f  eu.europa.ec.fisheries.uvms.movement.liquibase-${unionvms.project.movement.module}.sql >/dev/null
+psql -U movement -d $1 --host=$2 --single-transaction -q -f  eu.europa.ec.fisheries${unionvms.project.movement.module}.sql >/dev/null
 echo "Running rules.sql to create tables and init data"
-psql -U rules -d $1 --host=$2 -a --single-transaction -q -f  eu.europa.ec.fisheries.uvms.rules.liquibase-${unionvms.project.rules.module}.sql >/dev/null
+psql -U rules -d $1 --host=$2 -a --single-transaction -q -f  eu.europa.ec.fisheries${unionvms.project.rules.module}.sql >/dev/null
 echo "Running movementrules.sql to create tables and init data"
 psql -U movementrules -d $1 --host=$2 -a --single-transaction -q -f  eu.europa.ec.fisheries.uvms.movement-rules.liquibase-${unionvms.project.movementrules.module}.sql >/dev/null
 echo "Running usm.sql to create tables and init data"
-psql -U usm -d $1 --host=$2 --single-transaction -q -f eu.europa.ec.fisheries.uvms.user.liquibase-${unionvms.project.user.liquibase.version}.sql >/dev/null
+psql -U usm -d $1 --host=$2 --single-transaction -q -f eu.europa.ec.fisheries${unionvms.project.user.liquibase.version}.sql >/dev/null
 echo "Running spatial.sql to create tables and init data"
-psql -U spatial -d $1 --host=$2 --single-transaction -q -f eu.europa.ec.fisheries.uvms.spatial.liquibase-${unionvms.project.spatial.module}.sql >/dev/null
+psql -U spatial -d $1 --host=$2 --single-transaction -q -f eu.europa.ec.fisheries${unionvms.project.spatial.module}.sql >/dev/null
 echo "Running mdr.sql to create tables and init data"
-psql -U mdr -d $1 --host=$2 --single-transaction -q -f eu.europa.ec.fisheries.uvms.mdr.liquibase-${unionvms.project.mdr.module}.sql >/dev/null
+psql -U mdr -d $1 --host=$2 --single-transaction -q -f eu.europa.ec.fisheries${unionvms.project.mdr.module}.sql >/dev/null
 echo "Running activity.sql to create tables and init data"
-psql -U activity -d $1 --host=$2 --single-transaction -q -f eu.europa.ec.fisheries.uvms.activity.liquibase-${unionvms.project.activity.module}.sql >/dev/null
+psql -U activity -d $1 --host=$2 --single-transaction -q -f eu.europa.ec.fisheries${unionvms.project.activity.module}.sql >/dev/null
 echo "Running reporting.sql to create tables and init data"
-psql -U reporting -d $1 --host=$2 --single-transaction -q -f eu.europa.ec.fisheries.uvms.reporting.liquibase-${unionvms.project.reporting.module}.sql >/dev/null
+psql -U reporting -d $1 --host=$2 --single-transaction -q -f eu.europa.ec.fisheries${unionvms.project.reporting.module}.sql >/dev/null
 echo "Running subscription.sql to create tables and init data"
-psql -U subscription -d $1 --host=$2 --single-transaction -q -f eu.europa.ec.fisheries.uvms.subscription.liquibase-${unionvms.project.subscription.module}.sql >/dev/null
+psql -U subscription -d $1 --host=$2 --single-transaction -q -f eu.europa.ec.fisheries${unionvms.project.subscription.module}.sql >/dev/null
 
 echo "Completed module.sql"
