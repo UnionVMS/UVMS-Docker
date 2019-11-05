b4fc64be44578995a4ae741394f306803b851ead
Ansgar Kroger <1104644+ansgarkroger@users.noreply.github.com>
2019-11-05 11:54:06 +0100

    allow working without a lock table

--- wildfly-base/src/main/docker/start.sh
+++ wildfly-base/src/main/docker/start.sh
@@ -13,7 +13,7 @@ do
    	  printf "Database available. Wildfly starting.\n"
       break
    fi
-   locked=`psql -t -A -h postgres -p 5432 -U postgres -d db71u -c "select locked from usm.databasechangeloglock;" 2>/dev/null`
+   locked=`psql -t -A -h postgres -p 5432 -U postgres -d db71u -c "select count(*) from usm.databasechangelog;" 2>/dev/null`
    if [[ -z "${locked// }" ]];then
       locked="y"
    fi
