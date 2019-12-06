8c3e55fbfb010d95c8ed0ac15b520e9ccda6e414
Jonas E.Mattsson <jonas.mattsson@consid.se>
2019-12-06 10:31:56 +0100

    StreamCollector to 0.9.12 Asset to 6.4.0

--- unionvms-test/src/test/java/eu/europa/ec/fisheries/uvms/docker/validation/asset/AssetRestIT.java
+++ unionvms-test/src/test/java/eu/europa/ec/fisheries/uvms/docker/validation/asset/AssetRestIT.java
@@ -13,6 +13,7 @@ copy of the GNU General Public License along with the IFDM Suite. If not, see <h
 */
 package eu.europa.ec.fisheries.uvms.docker.validation.asset;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import eu.europa.ec.fisheries.uvms.asset.client.model.*;
 import eu.europa.ec.fisheries.uvms.asset.model.constants.AuditOperationEnum;
 import eu.europa.ec.fisheries.uvms.docker.validation.common.AbstractRest;
@@ -297,15 +298,26 @@ public class AssetRestIT extends AbstractRest {
     }
 
     @Test
-    public void addNoteToAsset() {
+    public void addNoteToAsset() throws JsonProcessingException {
         AssetDTO asset = AssetTestHelper.createTestAsset();
 
         Note note = new Note();
-        note.setActivityCode("1");
-        note.setDate(OffsetDateTime.now(ZoneOffset.UTC));
-        note.setNotes("apa");
+        note.setNote("apa");
+        note.setCreatedBy("Tester");
 
         Note createdNote = AssetTestHelper.createNoteForAsset(asset, note);
+
+        assertNotNull(createdNote.getId());
+    }
+
+    @Test
+    public void addNoteToAssetUsingTimestamp() throws JsonProcessingException {
+        AssetDTO asset = AssetTestHelper.createTestAsset();
+
+        String input = "{\"id\":null,\"assetId\":\"09ec5b78-a96f-47ea-8a01-6bc876a085fc\",\"createdOn\":1575545948,\"note\":\"apa\",\"createdBy\":\"Tester\"}";
+        input = input.replace("09ec5b78-a96f-47ea-8a01-6bc876a085fc", asset.getId().toString());
+
+        Note createdNote = AssetTestHelper.createNoteForAsset(input);
 
         assertNotNull(createdNote.getId());
     }
