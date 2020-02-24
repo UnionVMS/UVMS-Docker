b31e661ecbff7c3c60420fc3f0098cd2953e821d
peerik <peter.eriksson@havochvatten.se>
2020-02-24 15:12:08 +0100

    revert deserializer

--- unionvms-test/src/test/java/eu/europa/ec/fisheries/uvms/docker/validation/common/JacksonInstantDeserializer.java
+++ unionvms-test/src/test/java/eu/europa/ec/fisheries/uvms/docker/validation/common/JacksonInstantDeserializer.java
@@ -1,15 +1,18 @@
 package eu.europa.ec.fisheries.uvms.docker.validation.common;
 
-import java.io.IOException;
-import java.time.Instant;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
+import eu.europa.ec.fisheries.uvms.commons.date.DateUtils;
+
+import java.io.IOException;
+import java.time.Instant;
 
 public class JacksonInstantDeserializer extends JsonDeserializer<Instant> {
 
     @Override
     public Instant deserialize(JsonParser p, DeserializationContext ctx) throws IOException {
-        return Instant.ofEpochMilli(p.getLongValue());
+        String str = p.getText();
+        return DateUtils.stringToDate(str);
     }
 }
