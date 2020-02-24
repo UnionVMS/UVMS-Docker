8f33d7ca6327dbe4af752aa9cd060539326064f8
peerik <peter.eriksson@havochvatten.se>
2020-02-24 11:45:17 +0100

    updated deserializer/serializer

--- unionvms-test/src/test/java/eu/europa/ec/fisheries/uvms/docker/validation/common/JacksonInstantDeserializer.java
+++ unionvms-test/src/test/java/eu/europa/ec/fisheries/uvms/docker/validation/common/JacksonInstantDeserializer.java
@@ -1,18 +1,15 @@
 package eu.europa.ec.fisheries.uvms.docker.validation.common;
 
+import java.io.IOException;
+import java.time.Instant;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
-import eu.europa.ec.fisheries.uvms.commons.date.DateUtils;
-
-import java.io.IOException;
-import java.time.Instant;
 
 public class JacksonInstantDeserializer extends JsonDeserializer<Instant> {
 
     @Override
     public Instant deserialize(JsonParser p, DeserializationContext ctx) throws IOException {
-        String str = p.getText();
-        return DateUtils.stringToDate(str);
+        return Instant.ofEpochMilli(p.getLongValue());
     }
 }
--- unionvms-test/src/test/java/eu/europa/ec/fisheries/uvms/docker/validation/common/JacksonInstantSerializer.java
+++ unionvms-test/src/test/java/eu/europa/ec/fisheries/uvms/docker/validation/common/JacksonInstantSerializer.java
@@ -1,13 +1,10 @@
 package eu.europa.ec.fisheries.uvms.docker.validation.common;
 
-import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.SerializerProvider;
-import com.fasterxml.jackson.databind.ser.std.StdSerializer;
-import eu.europa.ec.fisheries.uvms.commons.date.DateUtils;
-
 import java.io.IOException;
 import java.time.Instant;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 
 public class JacksonInstantSerializer extends StdSerializer<Instant> {
 
@@ -24,6 +21,6 @@ public class JacksonInstantSerializer extends StdSerializer<Instant> {
             Instant value, JsonGenerator jgen, SerializerProvider provider)
             throws IOException {
 
-        jgen.writeString(DateUtils.dateToEpochMilliseconds(value));
+        jgen.writeNumber(value.toEpochMilli());
     }
 }
