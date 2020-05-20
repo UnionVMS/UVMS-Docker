d475e1d7cc260313f6ca2c52395a3e9199a5b259
peerik <peter.eriksson@havochvatten.se>
2020-05-20 17:06:33 +0200

    import incident model

--- unionvms-test/src/test/java/eu/europa/ec/fisheries/uvms/docker/validation/incident/IncidentRestIT.java
+++ unionvms-test/src/test/java/eu/europa/ec/fisheries/uvms/docker/validation/incident/IncidentRestIT.java
@@ -1,13 +1,13 @@
 package eu.europa.ec.fisheries.uvms.docker.validation.incident;
 
 import eu.europa.ec.fisheries.schema.movementrules.ticket.v1.TicketStatusType;
-import eu.europa.ec.fisheries.schema.movementrules.ticket.v1.TicketType;
 import eu.europa.ec.fisheries.uvms.asset.client.model.AssetDTO;
 import eu.europa.ec.fisheries.uvms.docker.validation.asset.AssetTestHelper;
 import eu.europa.ec.fisheries.uvms.docker.validation.common.AbstractRest;
 import eu.europa.ec.fisheries.uvms.docker.validation.common.MessageHelper;
-import eu.europa.ec.fisheries.uvms.docker.validation.incident.dto.IncidentDto;
-import eu.europa.ec.fisheries.uvms.docker.validation.incident.dto.IncidentLogDto;
+import eu.europa.ec.fisheries.uvms.incident.model.dto.IncidentDto;
+import eu.europa.ec.fisheries.uvms.incident.model.dto.IncidentLogDto;
+import eu.europa.ec.fisheries.uvms.incident.model.dto.IncidentTicketDto;
 import org.junit.Test;
 
 import javax.ws.rs.core.GenericType;
@@ -27,7 +27,7 @@ public class IncidentRestIT extends AbstractRest {
     @Test
     public void createAssetNotSendingIncidentTest() throws Exception {
         AssetDTO asset = AssetTestHelper.createAsset(AssetTestHelper.createBasicAsset());
-        TicketType ticket = createTicket(asset.getId());
+        IncidentTicketDto ticket = createTicket(asset.getId());
         IncidentDto dto = createAssetNotSendingIncident(ticket, INCIDENT_CREATE);
         assertNotNull(dto);
     }
@@ -36,7 +36,7 @@ public class IncidentRestIT extends AbstractRest {
     public void getAssetNotSendingListTest() throws Exception {
         List<IncidentDto> before = getAssetNotSendingIncidentList();
         AssetDTO asset = AssetTestHelper.createAsset(AssetTestHelper.createBasicAsset());
-        TicketType ticket = createTicket(asset.getId());
+        IncidentTicketDto ticket = createTicket(asset.getId());
         createAssetNotSendingIncident(ticket, INCIDENT_CREATE);
         List<IncidentDto> after = getAssetNotSendingIncidentList();
         assertEquals(before.size() + 1, after.size());
@@ -45,12 +45,12 @@ public class IncidentRestIT extends AbstractRest {
     @Test
     public void updateAssetNotSendingStatusTest() throws Exception {
         AssetDTO asset = AssetTestHelper.createAsset(AssetTestHelper.createBasicAsset());
-        TicketType ticket = createTicket(asset.getId());
+        IncidentTicketDto ticket = createTicket(asset.getId());
         IncidentDto created = createAssetNotSendingIncident(ticket, INCIDENT_CREATE);
 
         assertNotEquals("RESOLVED", created.getStatus());
 
-        ticket.setStatus(TicketStatusType.CLOSED);
+        ticket.setStatus(TicketStatusType.CLOSED.toString());
         IncidentDto updated = createAssetNotSendingIncident(ticket, INCIDENT_UPDATE);
 
         assertEquals("RESOLVED", updated.getStatus());
@@ -59,10 +59,10 @@ public class IncidentRestIT extends AbstractRest {
     @Test
     public void getAssetNotSendingEventChangesTest() throws Exception {
         AssetDTO asset = AssetTestHelper.createAsset(AssetTestHelper.createBasicAsset());
-        TicketType ticket = createTicket(asset.getId());
+        IncidentTicketDto ticket = createTicket(asset.getId());
         IncidentDto incident = createAssetNotSendingIncident(ticket, INCIDENT_CREATE);
 
-        ticket.setStatus(TicketStatusType.CLOSED);
+        ticket.setStatus(TicketStatusType.CLOSED.toString());
         createAssetNotSendingIncident(ticket, INCIDENT_UPDATE);
 
         List<IncidentLogDto> dtoList = getWebTarget()
@@ -75,13 +75,13 @@ public class IncidentRestIT extends AbstractRest {
         assertTrue(dtoList.size() > 0);
     }
 
-    private IncidentDto createAssetNotSendingIncident(TicketType ticket, String eventName) throws Exception {
+    private IncidentDto createAssetNotSendingIncident(IncidentTicketDto ticket, String eventName) throws Exception {
         sendMessage(ticket, eventName);
         LockSupport.parkNanos(5000000000L);
 
         return getWebTarget()
                 .path("incident/rest/incident/byTicketId")
-                .path(ticket.getGuid())
+                .path(ticket.getId().toString())
                 .request(MediaType.APPLICATION_JSON)
                 .header(HttpHeaders.AUTHORIZATION, getValidJwtToken())
                 .get(IncidentDto.class);
@@ -95,27 +95,26 @@ public class IncidentRestIT extends AbstractRest {
                 .get(new GenericType<List<IncidentDto>>() {});
     }
 
-    private void sendMessage(TicketType ticket, String eventName) throws Exception {
+    private void sendMessage(IncidentTicketDto ticket, String eventName) throws Exception {
         try (MessageHelper messageHelper = new MessageHelper()) {
             String asString = OBJECT_MAPPER.writeValueAsString(ticket);
             messageHelper.sendMessageWithProperty(QUEUE_NAME, asString, eventName);
         }
     }
 
-    private TicketType createTicket(UUID assetId) {
-        TicketType ticket = new TicketType();
-        ticket.setGuid(UUID.randomUUID().toString());
-        ticket.setAssetGuid(assetId.toString());
-        ticket.setMovementGuid(UUID.randomUUID().toString());
-        ticket.setMobileTerminalGuid(UUID.randomUUID().toString());
+    private IncidentTicketDto createTicket(UUID assetId) {
+        IncidentTicketDto ticket = new IncidentTicketDto();
+        ticket.setId(UUID.randomUUID());
+        ticket.setAssetId(assetId.toString());
+        ticket.setMovementId(UUID.randomUUID().toString());
+        ticket.setMobTermId(UUID.randomUUID().toString());
         ticket.setRuleName("Asset not sending");
         ticket.setRuleGuid("Asset not sending");
         ticket.setUpdatedBy("UVMS");
-        ticket.setStatus(TicketStatusType.POLL_PENDING);
+        ticket.setStatus(TicketStatusType.POLL_PENDING.value());
         ticket.setTicketCount(1L);
-        String date = String.valueOf(Instant.now().getEpochSecond());
-        ticket.setOpenDate(date);
-        ticket.setUpdated(date);
+        ticket.setCreatedDate(Instant.now());
+        ticket.setUpdated(Instant.now());
         return ticket;
     }
 }
