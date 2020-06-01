43a65fce3dd03834c0745b9de63df345b8a41cae
Jonas E.Mattsson <jonas.mattsson@consid.se>
2020-06-01 14:35:15 +0200

    changed datastrukture in tests to match what is actually sent

--- unionvms-test/src/test/java/eu/europa/ec/fisheries/uvms/docker/validation/webgateway/IncidentCollectionIT.java
+++ unionvms-test/src/test/java/eu/europa/ec/fisheries/uvms/docker/validation/webgateway/IncidentCollectionIT.java
@@ -9,6 +9,7 @@ import eu.europa.ec.fisheries.uvms.docker.validation.incident.IncidentTestHelper
 import eu.europa.ec.fisheries.uvms.docker.validation.webgateway.dto.ExtendedIncidentLogDto;
 import eu.europa.ec.fisheries.uvms.docker.validation.webgateway.dto.NoteAndIncidentDto;
 import eu.europa.ec.fisheries.uvms.incident.model.dto.IncidentDto;
+import eu.europa.ec.fisheries.uvms.incident.model.dto.IncidentLogDto;
 import eu.europa.ec.fisheries.uvms.incident.model.dto.IncidentTicketDto;
 import eu.europa.ec.fisheries.uvms.incident.model.dto.enums.EventTypeEnum;
 import org.junit.Test;
@@ -61,17 +62,19 @@ public class IncidentCollectionIT extends AbstractRest {
         assertTrue(json.contains(noteAdded.getNote().getNote()));
         assertTrue(json.contains(noteAdded.getNote().getAssetId().toString()));
 
-        List<ExtendedIncidentLogDto> incidentLogList = OBJECT_MAPPER.readValue(json, new TypeReference<List<ExtendedIncidentLogDto>>(){});
+        ExtendedIncidentLogDto incidentLogDto = OBJECT_MAPPER.readValue(json, ExtendedIncidentLogDto.class);
 
-        assertEquals(2, incidentLogList.size());
-        assertFalse(incidentLogList.isEmpty());
-        assertTrue(incidentLogList.stream().allMatch(dto -> dto.getIncidentLog() != null));
-        assertTrue(incidentLogList.stream().allMatch(dto -> dto.getIncidentLog().getIncidentId() == incidentDto.getId().longValue()));
-        Optional<ExtendedIncidentLogDto> noteIncidentLog = incidentLogList.stream().filter(dto -> dto.getIncidentLog().getEventType().equals(EventTypeEnum.NOTE_CREATED)).findAny();
+        assertEquals(2, incidentLogDto.getIncidentLogs().size());
+        assertTrue(incidentLogDto.getIncidentLogs().values().stream().allMatch(dto -> dto != null));
+        assertTrue(incidentLogDto.getIncidentLogs().values().stream().allMatch(dto -> dto.getIncidentId() == incidentDto.getId().longValue()));
+
+        Optional<IncidentLogDto> noteIncidentLog = incidentLogDto.getIncidentLogs().values().stream().filter(dto -> dto.getEventType().equals(EventTypeEnum.NOTE_CREATED)).findAny();
         assertTrue(noteIncidentLog.isPresent());
-        assertEquals(5, noteIncidentLog.get().getRelatedObject().size());
-        assertTrue(noteIncidentLog.get().getRelatedObject().get("assetId").equals(note.getAssetId().toString()));
-        assertTrue(noteIncidentLog.get().getRelatedObject().get("note").equals(note.getNote()));
+        Note incidentNote = incidentLogDto.getNotes().get(noteIncidentLog.get().getRelatedObjectId().toString());
+        assertTrue(incidentNote != null);
+
+        assertTrue(incidentNote.getAssetId().equals(note.getAssetId()));
+        assertTrue(incidentNote.getNote().equals(note.getNote()));
 
 
     }
--- unionvms-test/src/test/java/eu/europa/ec/fisheries/uvms/docker/validation/webgateway/dto/ExtendedIncidentLogDto.java
+++ unionvms-test/src/test/java/eu/europa/ec/fisheries/uvms/docker/validation/webgateway/dto/ExtendedIncidentLogDto.java
@@ -1,36 +1,51 @@
 package eu.europa.ec.fisheries.uvms.docker.validation.webgateway.dto;
 
+import eu.europa.ec.fisheries.uvms.asset.client.model.Note;
+import eu.europa.ec.fisheries.uvms.docker.validation.movement.model.MicroMovement;
 import eu.europa.ec.fisheries.uvms.incident.model.dto.IncidentLogDto;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class ExtendedIncidentLogDto {
 
-    private IncidentLogDto incidentLog;
+    private Map<Long, IncidentLogDto> incidentLogs = new HashMap<>();
 
-    private Map<String, String> relatedObject;
+    private Map<String, Note> notes = new HashMap<>();
 
-    public ExtendedIncidentLogDto() {
+    private Map<String, Object> polls = new HashMap<>();
+
+    private Map<String, MicroMovement> manualPositions = new HashMap<>();
+
+    public Map<Long, IncidentLogDto> getIncidentLogs() {
+        return incidentLogs;
     }
 
-    public ExtendedIncidentLogDto(IncidentLogDto incidentLog, Map<String, String> relatedObject) {
-        this.incidentLog = incidentLog;
-        this.relatedObject = relatedObject;
+    public void setIncidentLogs(Map<Long, IncidentLogDto> incidentLogs) {
+        this.incidentLogs = incidentLogs;
     }
 
-    public IncidentLogDto getIncidentLog() {
-        return incidentLog;
+    public Map<String, Note> getNotes() {
+        return notes;
     }
 
-    public void setIncidentLog(IncidentLogDto incidentLog) {
-        this.incidentLog = incidentLog;
+    public void setNotes(Map<String, Note> notes) {
+        this.notes = notes;
     }
 
-    public Map<String, String> getRelatedObject() {
-        return relatedObject;
+    public Map<String, Object> getPolls() {
+        return polls;
     }
 
-    public void setRelatedObject(Map<String, String> relatedObject) {
-        this.relatedObject = relatedObject;
+    public void setPolls(Map<String, Object> polls) {
+        this.polls = polls;
+    }
+
+    public Map<String, MicroMovement> getManualPositions() {
+        return manualPositions;
+    }
+
+    public void setManualPositions(Map<String, MicroMovement> manualPositions) {
+        this.manualPositions = manualPositions;
     }
 }
